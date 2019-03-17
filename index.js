64b833af7598ca403d229cf2c8415e51a10f1a65
Thabang Mabula <1364103@students.wits.ac.za>
2019-03-17 23:40:04 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let path = require('path');
+let express = require('express');
+let app = express();
+let mainRouter = require('./mainRoutes.js');
+
+
+app.use(mainRouter);
+let port = process.env.PORT || 3000;
+app.listen(port);
+console.log("Express server running on port", port);
+ 
